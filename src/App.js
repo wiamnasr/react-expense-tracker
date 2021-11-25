204e5d14f11e0d391010444820c1295bb2e026df
Wiam <wiamnasr@gmail.com>
2021-11-25 12:59:31 +0000

    pushing changes 1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+// Components
+import Expenses from "./Components/Expenses-Components/Expenses/Expenses";
 
-function App() {
+const App = () => {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Car Insurance",
+      amount: 294.66,
+      date: new Date(2021, 10, 28),
+    },
+    {
+      id: "e2",
+      title: "Mortgage Payment",
+      amount: 500.42,
+      date: new Date(2021, 11, 23),
+    },
+    {
+      id: "e3",
+      title: "Bike Repair",
+      amount: 94.02,
+      date: new Date(2021, 10, 18),
+    },
+    {
+      id: "e4",
+      title: "Ice Cream",
+      amount: 14.44,
+      date: new Date(2021, 11, 1),
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Expense Tracker</h1>
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
